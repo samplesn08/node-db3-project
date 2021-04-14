337b5aa3336e9b79c08bacba89ffc309c4eae23d
samplesn08 <samplesn08@gmail.com>
2021-04-14 11:25:35 -0400

    middleware complete

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -7,8 +7,13 @@
   }
 */
 const checkSchemeId = (req, res, next) => {
-
-}
+  const { id } = req.params;
+  if(!id){
+    res.status(404).json({ message: `scheme with scheme_id ${id} not found` })
+  }else{
+    next();
+  }
+};
 
 /*
   If `scheme_name` is missing, empty string or not a string:
@@ -19,8 +24,13 @@ const checkSchemeId = (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
-}
+  const { name } = req.body;
+  if(!name || name===""){
+    res.status(400).json({ message: "invalid scheme_name" })
+  }else{
+    next();
+  }
+};
 
 /*
   If `instructions` is missing, empty string or not a string, or
@@ -32,7 +42,13 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
-
+  const inst = req.body.instructions;
+  const num = req.body.step_number;
+  if(!inst || inst==="" || !num || num<1){
+    res.status(400).json({ message: "invalid step" })
+  }else{
+    next();
+  }
 }
 
 module.exports = {
